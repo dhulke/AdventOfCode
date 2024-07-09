f700b61e43e37e1cb1928f580a03430099d6fae8
danilo.moraes <danilo.moraes@trydatabook.com>
2024-07-08 21:04:02 -0300

    year 2023 day 3 elixir implementation. Needs refactoring

--- elixir/lib/aoc/year2023/day3/day3.ex
+++ elixir/lib/aoc/year2023/day3/day3.ex
@@ -14,13 +14,42 @@ defmodule Aoc.Year2023.Day3.Day3 do
     input
     |> TextUtil.lines()
     |> Enum.with_index()
-    |> Enum.map(&(numbers_adjacent_to_symbols(number_coordinates, &1)))
+    |> Enum.map(&numbers_adjacent_to_symbols(number_coordinates, &1))
     |> List.flatten()
     |> Enum.uniq()
-    |> IO.inspect()
-    |> Stream.map(fn {_, _, number} -> number end)
+    |> Stream.map(fn {_, _, number, _, _} -> number end)
     |> Stream.map(&String.to_integer/1)
     |> Enum.sum()
+  end
+
+  def part2(input) do
+    number_coordinates =
+      input
+      |> TextUtil.lines()
+      |> Enum.with_index()
+      |> Enum.map(&number_coordinates_to_map/1)
+      |> Enum.reduce(%{}, &Map.merge/2)
+
+    input
+    |> TextUtil.lines()
+    |> Enum.with_index()
+    |> Enum.map(&gear_ratios(number_coordinates, &1))
+    |> List.flatten()
+    |> Enum.sum()
+  end
+
+  defp gear_ratios(numbers_coordinates, {line, line_index}) do
+    line
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.filter(fn {grapheme, _} -> grapheme === "*" end)
+    |> Enum.map(fn {_, col_index} ->
+      lookup_numbers_adjacent_to(numbers_coordinates, col_index, line_index)
+    end)
+    |> Enum.filter(fn adjacent_numbers -> length(adjacent_numbers) === 2 end)
+    |> Enum.map(fn [{_, _, number1, _, _}, {_, _, number2, _, _}] ->
+      String.to_integer(number1) * String.to_integer(number2)
+    end)
   end
 
   defp numbers_adjacent_to_symbols(numbers_coordinates, {line, line_index}) do
@@ -30,24 +59,28 @@ defmodule Aoc.Year2023.Day3.Day3 do
     |> Enum.filter(fn {grapheme, _} ->
       !TextUtil.is_digit(grapheme) and grapheme !== "."
     end)
-    |> Enum.map(fn {_, col_index} -> lookup_numbers_adjacent_to(numbers_coordinates, col_index, line_index) end)
+    |> Enum.map(fn {_, col_index} ->
+      lookup_numbers_adjacent_to(numbers_coordinates, col_index, line_index)
+    end)
     |> List.flatten()
   end
 
-  defp lookup_numbers_adjacent_to(number_coordinates, col_index, line_index) do
+  defp lookup_numbers_adjacent_to(number_coordinates, sym_col_index, sym_line_index) do
     [
-      {col_index - 1, line_index - 1},
-      {col_index, line_index - 1},
-      {col_index + 1, line_index - 1},
-      {col_index - 1, line_index},
-      {col_index, line_index},
-      {col_index + 1, line_index},
-      {col_index - 1, line_index + 1},
-      {col_index, line_index + 1},
-      {col_index + 1, line_index + 1}
+      {sym_col_index - 1, sym_line_index - 1},
+      {sym_col_index, sym_line_index - 1},
+      {sym_col_index + 1, sym_line_index - 1},
+      {sym_col_index - 1, sym_line_index},
+      {sym_col_index, sym_line_index},
+      {sym_col_index + 1, sym_line_index},
+      {sym_col_index - 1, sym_line_index + 1},
+      {sym_col_index, sym_line_index + 1},
+      {sym_col_index + 1, sym_line_index + 1}
     ]
-    |> Enum.map(&(Map.get(number_coordinates, &1)))
+    |> Enum.map(&Map.get(number_coordinates, &1))
     |> Enum.reject(&is_nil/1)
+    |> Enum.map(fn x -> x |> Tuple.append(sym_col_index) |> Tuple.append(sym_line_index) end)
+    |> Enum.uniq()
   end
 
   defp number_coordinates_to_map({line, line_index}) do
--- elixir/test/aoc/year2023/day3/day3_test.exs
+++ elixir/test/aoc/year2023/day3/day3_test.exs
@@ -18,27 +18,32 @@ defmodule AocTest.Year2023.Day3.Day3 do
     assert Aoc.Year2023.Day3.Day3.part1(sample_input) == 4361
   end
 
-#  test "Part 1 - Real Payload" do
-#    contents = File.read!(Path.join(__DIR__, "part3_payload.txt"))
-#
-#    assert Aoc.Year2023.Day3.Day3.part1(contents) == 2256
-#  end
+  test "Part 1 - Real Payload" do
+    contents = File.read!(Path.join(__DIR__, "part3_payload.txt"))
 
-#  test "Part 2 - Example" do
-#    sample_input = """
-#    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-#    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-#    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-#    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-#    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-#    """
-#
-#    assert Aoc.Year2023.Day2.Day2.part2(sample_input) == 2286
-#  end
-#
-#  test "Part 2 - Real Payload" do
-#    contents = File.read!(Path.join(__DIR__, "part2_payload.txt"))
-#
-#    assert Aoc.Year2023.Day2.Day2.part2(contents) == 74229
-#  end
+    assert Aoc.Year2023.Day3.Day3.part1(contents) == 530849
+  end
+
+  test "Part 2 - Example" do
+    sample_input = """
+    467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..
+    """
+
+    assert Aoc.Year2023.Day3.Day3.part2(sample_input) == 467835
+  end
+
+  test "Part 2 - Real Payload" do
+    contents = File.read!(Path.join(__DIR__, "part3_payload.txt"))
+
+    assert Aoc.Year2023.Day3.Day3.part2(contents) == 74229
+  end
 end
